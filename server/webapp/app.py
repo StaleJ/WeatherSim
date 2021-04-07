4c50e6adc7d507b8c2dcd2ff15509d4ff53bda7c
StaleJ <stale.jacobsen@student.uib.no>
2021-04-07 18:22:00 +0200

    Working on rest API

--- server/webapp/app.py
+++ server/webapp/app.py
@@ -9,9 +9,13 @@ api = Api(app)
 
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html', data=get_city())
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 def get_all_data() -> dict:
@@ -21,15 +25,34 @@ def get_all_data() -> dict:
     return json_object
 
 
+def get_last_temp_rain():
+    data = {}
+    all_data = get_all_data()
+    for city in all_data.keys():
+        data[city] = all_data[city.capitalize()]
+
+    return data
+
+
+def get_city():
+    list_ = []
+    all_data = get_all_data()
+    for city in all_data.keys():
+        list_.append(city)
+
+    return list_
+
+
 #  REST API
 
 class RainCity(Resource):
 
-    def get(self, city):
+    def get(self, city: str):
+        cap_city = city.capitalize()
         data = {}
         all_data = get_all_data()
-        for i in range(min(24, len(all_data[city]))):
-            day = all_data[city].popitem()
+        for i in range(min(24, len(all_data[cap_city]))):
+            day = all_data[cap_city].popitem()
             data[day[0]] = day[1]['Rain']
 
         return data
@@ -41,8 +64,14 @@ class Rain(Resource):
         data = {}
         all_data = get_all_data()
         for city in all_data.keys():
-            data[city] = RainCity.get(self,city)
+            data[city] = RainCity.get(self, city)
         return data
+
+
+class LastCity(Resource):
+
+    def get(self):
+        return get_last_temp_rain()
 
 
 #
@@ -51,6 +80,7 @@ class Rain(Resource):
 
 api.add_resource(RainCity, '/rain/<city>')
 api.add_resource(Rain, '/rain')
+api.add_resource(LastCity, '/last')
 
 if __name__ == '__main__':
     app.run(debug=True)
