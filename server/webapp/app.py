4855e72447a6da26335062e585d3a2fce5658241
StaleJ <stale.jacobsen@student.uib.no>
2021-04-07 17:24:41 +0200

    Added REST API commands /rain and /rain/city

--- server/webapp/app.py
+++ server/webapp/app.py
@@ -1,10 +1,56 @@
-from flask import Flask
-from flask import render_template
+import json
+
+from flask import Flask, render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+DATA = "../data.json"
+api = Api(app)
 
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
+
+
+def get_all_data() -> dict:
+    with open(DATA, "r") as file:
+        json_object = json.loads(file.read())
+    file.close()
+    return json_object
+
+
+#  REST API
+
+class RainCity(Resource):
+
+    def get(self, city):
+        data = {}
+        all_data = get_all_data()
+        for i in range(min(24, len(all_data[city]))):
+            day = all_data[city].popitem()
+            data[day[0]] = day[1]['Rain']
+
+        return data
+
+
+class Rain(Resource):
+
+    def get(self):
+        data = {}
+        all_data = get_all_data()
+        for city in all_data.keys():
+            data[city] = RainCity.get(self,city)
+        return data
+
+
+#
+# Actually setup the Api resource routing here
+#
+
+api.add_resource(RainCity, '/rain/<city>')
+api.add_resource(Rain, '/rain')
+
+if __name__ == '__main__':
+    app.run(debug=True)
