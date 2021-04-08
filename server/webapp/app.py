bca40f88766e605381c82c5328c6643d915c6f15
StaleJ <stale.jacobsen@student.uib.no>
2021-04-08 16:47:44 +0200

    REST API and Flask APP somewhat done

--- server/webapp/app.py
+++ server/webapp/app.py
@@ -1,11 +1,26 @@
 import json
 
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
 from flask import Flask, render_template
+from flask_apispec import doc
+from flask_apispec.extension import FlaskApiSpec
+from flask_apispec.views import MethodResource
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 DATA = "../data.json"
 api = Api(app)
+app.config.update({'APISPEC_SPEC': APISpec(
+    title="Group30's WeatherAPP",
+    version='v1',
+    plugins=[MarshmallowPlugin()],
+    openapi_version='2.0.0'
+),
+    'APISPEC_SWAGGER_URL': '/swagger/',
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'})
+
+docs = FlaskApiSpec(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -34,50 +49,58 @@ def get_city() -> list:  # Return
     return list_
 
 
+def get_request(city, updates=24):
+    cap_city = city.capitalize()
+    data = {}
+    all_data = get_all_data()
+    for i in range(min(updates, len(all_data[cap_city]))):
+        day = all_data[cap_city].popitem()
+        data[day[0]] = day[1]
+
+    return data
+
 # REST API
 
-class City(Resource):
 
+class City(MethodResource,Resource):
+
+    @doc(description="Gives data for the last 24h for specific city", tags=["GET"])
     def get(self, city, updates=24):
-        cap_city = city.capitalize()
-        data = {}
-        all_data = get_all_data()
-        for i in range(min(updates, len(all_data[cap_city]))):
-            day = all_data[cap_city].popitem()
-            data[day[0]] = day[1]
-
-        return data
+        return get_request(city)
 
 
-class Rain(Resource):
+class AllData(MethodResource, Resource):
 
+    @doc(description="Gives data for the last 24h for all cities", tags=["GET"])
     def get(self):
         data = {}
         all_data = get_all_data()
         for city in all_data.keys():
-            data[city] = City.get(self, city)
+            data[city] = get_request(city)
         return data
 
 
-class Last(Resource):
+class Last(MethodResource, Resource):
 
+    @doc(description="Give last measurement for all cities", tags=["GET"])
     def get(self):
         data = {}
         all_data = get_all_data()
         for city in all_data.keys():
-            data[city] = City.get(self, city, 1)
+            data[city] = get_request(city,1)
 
         return data
 
 
-class LastCity(Resource):
+class LastCity(MethodResource,Resource):
 
+    @doc(description="Give last measurement for specific city", tags=["GET"])
     def get(self, city):
-        return {city: City.get(self, city, 1)}
+        return {city: get_request(city, 1)}
 
 
-class All(Resource):
-
+class All(MethodResource, Resource):
+    @doc(description="Give last measurement for all cities",tags=["GET"])
     def get(self):
         return get_all_data()
 
@@ -87,10 +110,15 @@ class All(Resource):
 #
 
 api.add_resource(City, '/<city>')  # gives data for the last 24h for specific city
-api.add_resource(Rain, '/data')  # gives data for the last 24h for all cities
+api.add_resource(AllData, '/data')  # gives data for the last 24h for all cities
 api.add_resource(Last, '/last')  # give last measurement for all cities
 api.add_resource(LastCity, '/<city>/last')  # give last measurement for specific city
 api.add_resource(All, '/all')  # Returns all data in the database
+docs.register(City)
+docs.register(AllData)
+docs.register(Last)
+docs.register(LastCity)
+docs.register(All)
 
 if __name__ == '__main__':
     app.run(debug=True)
